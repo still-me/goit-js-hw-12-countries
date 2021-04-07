4c98b6d50650479c39155edffe6912d69b7460f2
Vladyslav Fomenko <71770160+still-me@users.noreply.github.com>
2021-04-07 16:58:04 +0300

    add .catch() at fechCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,9 @@
+import { fetchError } from './errors';
+
 export default function fetchCountries(countryName) {
     const url = `https://restcountries.eu/rest/v2/name/${countryName}`
 
     return fetch(url)
-    .then(response => response.json())
+        .then(response => response.json())
+        .catch(fetchError)
 }
